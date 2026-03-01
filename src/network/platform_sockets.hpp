4db556feb6157b9e359212f68b0b6e446d7f6f7d
Branden Call <brandencall@live.com>
2026-03-01 14:02:41 -0700

    I hate windows sometimes

--- src/network/platform_sockets.hpp
+++ src/network/platform_sockets.hpp
@@ -2,7 +2,10 @@
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#define WIN32_LEAN_AND_MEAN
+#define NOGDI
+#define NOUSER
+#define NOMINMAX
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "Ws2_32.lib")
