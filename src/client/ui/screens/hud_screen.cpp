72f2fbc1fb867779e36210526d50f451af314a0f
Branden Call <brandencall@live.com>
2026-04-01 10:51:48 -0600

    Added Current number of walls to hud

--- src/client/ui/screens/hud_screen.cpp
+++ src/client/ui/screens/hud_screen.cpp
@@ -7,12 +7,15 @@ HudScreen::HudScreen(const state::PlayerState &localPlayer) : m_localPlayer(loca
 
 void HudScreen::Render() {
     int h = GetScreenHeight();
-
     std::string health = "HP: " + std::to_string(static_cast<int>(m_localPlayer.health));
+    std::string currentWalls = "Walls: " + std::to_string(static_cast<int>(m_localPlayer.currentAvaliableWalls));
     int padding = 10;
     int fontSize = 24;
+    int gap = 20;
 
     DrawText(health.c_str(), padding, h - fontSize - padding, fontSize, RAYWHITE);
+    DrawText(currentWalls.c_str(), padding + MeasureText(health.c_str(), fontSize) + gap, h - fontSize - padding,
+             fontSize, RAYWHITE);
 }
 
 } // namespace UI
